b995fe8915d02c9492e00d7977b003592ae62ff4
Krzychu142 <kustosz142@gmail.com>
2024-11-04 19:34:00 +0100

    init commit

---
+++ src/main/java/pk/ztp/filmbase/FilmbaseApplication.java
@@ -0,0 +1,13 @@
+package pk.ztp.filmbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmbaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pk/ztp/filmbase/FilmbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package pk.ztp.filmbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
