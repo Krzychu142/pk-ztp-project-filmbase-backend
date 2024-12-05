04c04e26b251c700fd330c697e2ace2de813292d
Krzychu142 <kustosz142@gmail.com>
2024-12-05 21:11:56 +0100

    Created CommentRepository. Injected it to service CommentService.

---
+++ src/main/java/pk/ztp/filmbase/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package pk.ztp.filmbase.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pk.ztp.filmbase.model.Comment;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
