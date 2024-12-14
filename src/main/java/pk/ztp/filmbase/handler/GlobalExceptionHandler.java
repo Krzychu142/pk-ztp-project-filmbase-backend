26b131ded181d5b878154e85f43d9e725a61cfa3
Krzychu142 <kustosz142@gmail.com>
2024-12-14 20:35:33 +0100

    DIRECT MASTER PUSH - added simple script for db inserting with films, added missing import steatment, updated .gitignore

--- src/main/java/pk/ztp/filmbase/handler/GlobalExceptionHandler.java
+++ src/main/java/pk/ztp/filmbase/handler/GlobalExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import pk.ztp.filmbase.dto.ApiResponseDTO;
+import pk.ztp.filmbase.exception.ResourceNotFound;
 import pk.ztp.filmbase.exception.UserAlreadyExistsException;
 
 import java.util.HashMap;
