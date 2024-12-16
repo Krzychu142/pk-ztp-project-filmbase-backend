97d230278554f26a285acf76d34ff926f1979dd9
Krzychu142 <kustosz142@gmail.com>
2024-12-16 18:33:17 +0100

    Changed import from implementation into interface.

--- src/main/java/pk/ztp/filmbase/controller/CommentController.java
+++ src/main/java/pk/ztp/filmbase/controller/CommentController.java
@@ -10,14 +10,14 @@ import org.springframework.web.bind.annotation.*;
 import pk.ztp.filmbase.dto.ApiResponseDTO;
 import pk.ztp.filmbase.dto.CommentDTO;
 import pk.ztp.filmbase.security.IAuthenticationFacade;
-import pk.ztp.filmbase.service.CommentService;
+import pk.ztp.filmbase.service.ICommentService;
 
 @RestController
 @RequestMapping("/comments")
 @RequiredArgsConstructor
 public class CommentController {
     private final IAuthenticationFacade authenticationFacade;
-    private final CommentService commentService;
+    private final ICommentService commentService;
 
     @PostMapping("/comment")
     public ResponseEntity<ApiResponseDTO> saveComment(@Valid @RequestBody CommentDTO commentDTO) {
