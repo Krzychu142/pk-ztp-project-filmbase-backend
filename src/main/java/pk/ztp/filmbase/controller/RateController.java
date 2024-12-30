fce8ecb77a54aa9c107a3caba16c54a9b5379245
Krzychu142 <kustosz142@gmail.com>
2024-12-30 21:25:55 +0100

    feat(rate): created TODO blueprint

---
+++ src/main/java/pk/ztp/filmbase/controller/RateController.java
@@ -0,0 +1,17 @@
+package pk.ztp.filmbase.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rates")
+public class RateController {
+
+    /**
+     * TODO:
+     * 1. RATE FILM
+     * 2. GET ALL RATES BY FILM ID
+     * 3. GET AVERAGE OF RATES BY FILM ID
+     * */
+
+}
